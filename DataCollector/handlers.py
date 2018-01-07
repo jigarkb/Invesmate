440b665aace72a4f8bcc627e3a367347944c1d86
Jigar Bhatt <jigarbhatt93@gmail.com>
2018-01-07 09:19:02 -0800

    handling error so that it never throws server error

--- DataCollector/handlers.py
+++ DataCollector/handlers.py
@@ -1,5 +1,8 @@
+import logging
 import time
 import datetime
+import traceback
+
 import webapp2
 from google.appengine.api import taskqueue
 
@@ -9,82 +12,91 @@ import utils
 
 class USMarketHandler(webapp2.RequestHandler):
     def update(self):
-        if datetime.datetime.now().weekday() > 4:
-            return
-        if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
-                taskqueue.add(
-                    method='POST',
-                    queue_name='default',
-                    url='/data_collector/us_market/update',
-                )
+        try:
+            if datetime.datetime.now().weekday() > 4:
+                return
+            if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
+                    taskqueue.add(
+                        method='POST',
+                        queue_name='default',
+                        url='/data_collector/us_market/update',
+                    )
 
-        else:
-            update = False
-            if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
-                update = True
             else:
-                user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
-                if user:
+                update = False
+                if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
                     update = True
+                else:
+                    user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
+                    if user:
+                        update = True
 
-            if update:
-                start_time = time.time()
-                while True:
-                    if time.time() - start_time > 9 * 60:
-                        break
-                    USMarket().update()
-                    time.sleep(1)
+                if update:
+                    start_time = time.time()
+                    while True:
+                        if time.time() - start_time > 9 * 60:
+                            break
+                        USMarket().update()
+                        time.sleep(1)
+        except:
+            logging.error(traceback.format_exc())
 
 
 class CoinbaseMarketHandler(webapp2.RequestHandler):
     def update(self):
-        if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
-            taskqueue.add(
-                method='POST',
-                queue_name='default',
-                url='/data_collector/coinbase_market/update',
-            )
+        try:
+            if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
+                taskqueue.add(
+                    method='POST',
+                    queue_name='default',
+                    url='/data_collector/coinbase_market/update',
+                )
 
-        else:
-            update = False
-            if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
-                update = True
             else:
-                user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
-                if user:
+                update = False
+                if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
                     update = True
+                else:
+                    user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
+                    if user:
+                        update = True
 
-            if update:
-                start_time = time.time()
-                while True:
-                    if time.time() - start_time > 9 * 60:
-                        break
-                    CoinbaseMarket().update()
-                    time.sleep(1)
+                if update:
+                    start_time = time.time()
+                    while True:
+                        if time.time() - start_time > 9 * 60:
+                            break
+                        CoinbaseMarket().update()
+                        time.sleep(1)
+        except:
+            logging.error(traceback.format_exc())
 
 
 class BinanceMarketHandler(webapp2.RequestHandler):
     def update(self):
-        if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
-            taskqueue.add(
-                method='POST',
-                queue_name='default',
-                url='/data_collector/binance_market/update',
-            )
+        try:
+            if self.request.method == 'GET' and "X-AppEngine-Cron" in self.request.headers:
+                taskqueue.add(
+                    method='POST',
+                    queue_name='default',
+                    url='/data_collector/binance_market/update',
+                )
 
-        else:
-            update = False
-            if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
-                update = True
             else:
-                user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
-                if user:
+                update = False
+                if self.request.method == 'POST' and "X-AppEngine-QueueName" in self.request.headers:
                     update = True
+                else:
+                    user = utils.authenticate_user(self, self.request.url, ['jigarbhatt93@gmail.com'])
+                    if user:
+                        update = True
 
-            if update:
-                start_time = time.time()
-                while True:
-                    if time.time() - start_time > 9 * 60:
-                        break
-                    BinanceMarket().update()
-                    time.sleep(1)
+                if update:
+                    start_time = time.time()
+                    while True:
+                        if time.time() - start_time > 9 * 60:
+                            break
+                        BinanceMarket().update()
+                        time.sleep(1)
+        except:
+            logging.error(traceback.format_exc())
